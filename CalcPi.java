1206ad0523d8677a7bf1168c32161f31c1ee2e02
Harel <harel@Harels-MacBook-Pro.local>
2025-11-05 17:42:26 +0200

    3rd attempt

--- CalcPi.java
+++ CalcPi.java
@@ -3,7 +3,7 @@ public class CalcPi {
 	public static void main(String[] args) {
 		// Replace this comment with your code
 		int num = Integer.parseInt(args[0]);
-		System.out.println("pi according to Java:" + Math.PI);
+		System.out.println("pi according to Java: " + Math.PI);
 		double somewhatPI = 0.0;
 		int j = 1;
 		for (int i = 0; i < num; i++) {
